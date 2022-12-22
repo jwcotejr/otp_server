19d5c59d15fe4d236c37e57c1fb72a550b44277e
John Cote <jwcotejr@gmail.com>
2022-12-22 12:49:47 -0500

    otp: Create Message Director (again)

--- otp/main.py
+++ otp/main.py
@@ -3,6 +3,7 @@ from panda3d.direct import DCFile
 from direct.task.TaskManagerGlobal import taskMgr
 
 from otp.config.Config import Config
+from otp.messagedirector.MessageDirector import MessageDirector
 from otp.core import ServiceFactory
 
 import builtins
@@ -19,10 +20,17 @@ builtins.config = Config('config/config.json')
 builtins.dcFile = DCFile()
 
 # Now, read our DC files specified in the config into the global DC file:
-for path in config.get('dc-files', []):
+for path in config.get('general', {}).get('dc-files', []):
     dcFile.read(path)
 
-# Create all of our services:
+# If the Message Director is defined in the config, create it first:
+mdConfig = config.get('messagedirector', {})
+if mdConfig:
+    mdHost = mdConfig['host']
+    mdPort = mdConfig['port']
+    MessageDirector(mdHost, mdPort)
+
+# Now, create all of our services:
 for serviceConfig in config.get('services', []):
     serviceName = serviceConfig['type']
     ServiceFactory.createService(serviceName, serviceConfig)
--- otp/messagedirector/MessageDirector.py
+++ otp/messagedirector/MessageDirector.py
@@ -5,9 +5,3 @@ class MessageDirector(NetworkAcceptor):
 
     def __init__(self, host, port):
         NetworkAcceptor.__init__(self, host, port)
-
-    @staticmethod
-    def createFromConfig(config):
-        host = config['host']
-        port = config['port']
-        MessageDirector(host, port)
