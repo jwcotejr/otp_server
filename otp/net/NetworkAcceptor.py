26c2b47429b6ce3643b23783d0c025ff5d1dae17
John Cote <jwcotejr@gmail.com>
2022-12-22 01:36:09 -0500

    net: More NetworkAcceptor progress

--- otp/net/NetworkAcceptor.py
+++ otp/net/NetworkAcceptor.py
@@ -1,5 +1,5 @@
 from panda3d.core import QueuedConnectionManager, QueuedConnectionListener, QueuedConnectionReader
-from panda3d.core import ConnectionWriter
+from panda3d.core import ConnectionWriter, PointerToConnection, NetAddress
 
 from otp.core.UniqueObject import UniqueObject
 
@@ -20,3 +20,23 @@ class NetworkAcceptor(UniqueObject):
 
         taskMgr.add(self.listenerPollTask, self.uniqueName('listener-poll-task'))
         taskMgr.add(self.readerPollTask, self.uniqueName('reader-poll-task'))
+
+    def listenerPollTask(self, task):
+        # Listens for new connections (i.e. new clients)
+        if self.connectionListener.newConnectionAvailable():
+            rendezvous = PointerToConnection()
+            netAddress = NetAddress()
+            connection = PointerToConnection()
+            if self.connectionListener.getNewConnection(rendezvous, netAddress, connection):
+                # We got a new connection!
+
+                # Dereference:
+                connection = connection.p()
+
+                # Now we can start listening to our new connection:
+                self.connectionReader.addConnection(connection)
+
+                # Create the client instance for our new connection:
+                self.createClient(connection)
+
+        return task.cont
