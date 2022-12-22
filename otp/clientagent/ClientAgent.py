956e7a02660de88d2d9f533f385f29db2f1b489e
John Cote <jwcotejr@gmail.com>
2022-12-22 13:50:51 -0500

    otp: Actually create the Client Agent

--- otp/clientagent/ClientAgent.py
+++ otp/clientagent/ClientAgent.py
@@ -1,4 +1,20 @@
-class ClientAgent:
+from otp.net.NetworkAcceptor import NetworkAcceptor
+
+
+class ClientAgent(NetworkAcceptor):
+
+    def __init__(self, host, port, serverVersion, dcHash, channelMin, channelMax):
+        NetworkAcceptor.__init__(self, host, port)
+
     @staticmethod
     def createFromConfig(config):
-        pass  # TODO
+        # Get our config values:
+        host = config['host']
+        port = config['port']
+        serverVersion = config['server-version']
+        dcHash = config.get('dc-hash')
+        channelMin = config['channels']['min']
+        channelMax = config['channels']['max']
+
+        # Create our ClientAgent service:
+        ClientAgent(host, port, serverVersion, dcHash, channelMin, channelMax)
