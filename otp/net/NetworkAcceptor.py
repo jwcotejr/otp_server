49ba4435f9d8e4500d8ee5d93aa82cf80f18e8bd
John Cote <jwcotejr@gmail.com>
2022-12-22 00:14:00 -0500

    net: Current NetworkAcceptor work

--- otp/net/NetworkAcceptor.py
+++ otp/net/NetworkAcceptor.py
@@ -1,5 +1,22 @@
+from panda3d.core import QueuedConnectionManager, QueuedConnectionListener, QueuedConnectionReader
+from panda3d.core import ConnectionWriter
+
 from otp.core.UniqueObject import UniqueObject
 
 
 class NetworkAcceptor(UniqueObject):
-    pass
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+        self.connectionManager = QueuedConnectionManager()
+        self.connectionListener = QueuedConnectionListener(self.connectionManager, 0)
+        self.connectionReader = QueuedConnectionReader(self.connectionManager, 0)
+        self.connectionWriter = ConnectionWriter(self.connectionManager, 0)
+
+        self.tcpSocket = self.connectionManager.openTCPServerRendezvous(host, port, 1000)
+        self.connectionListener.addConnection(self.tcpSocket)
+
+        taskMgr.add(self.listenerPollTask, self.uniqueName('listener-poll-task'))
+        taskMgr.add(self.readerPollTask, self.uniqueName('reader-poll-task'))
