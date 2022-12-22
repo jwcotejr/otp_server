a8d584236b130649c044722818cf6b647630d356
John Cote <jwcotejr@gmail.com>
2022-12-21 23:12:27 -0500

    otp: Create services specified in config file

--- otp/main.py
+++ otp/main.py
@@ -3,6 +3,7 @@ from panda3d.direct import DCFile
 from direct.task.TaskManagerGlobal import taskMgr
 
 from otp.config.Config import Config
+from otp.core import ServiceFactory
 
 import builtins
 
@@ -20,6 +21,11 @@ builtins.dcFile = DCFile()
 # Now, read our DC files specified in the config into the global DC file:
 for path in config.get('dc-files', []):
     dcFile.read(path)
+
+# Create all of our services:
+for serviceConfig in config.get('services', []):
+    serviceName = serviceConfig['type']
+    ServiceFactory.createService(serviceName, serviceConfig)
 
 # We're good to go. Let's start the task manager:
 taskMgr.run()
