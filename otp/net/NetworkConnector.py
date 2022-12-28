4da2fd94e1572835d08a6ce9a41fd50d1ae0e1a7
John Cote <jwcotejr@gmail.com>
2022-12-27 20:09:28 -0500

    net: Work on NetworkConnector

--- otp/net/NetworkConnector.py
+++ otp/net/NetworkConnector.py
@@ -1,3 +1,6 @@
+from panda3d.core import QueuedConnectionManager, QueuedConnectionReader
+from panda3d.core import ConnectionWriter
+
 from otp.core.UniqueObject import UniqueObject
 
 
@@ -6,3 +9,42 @@ class NetworkConnector(UniqueObject):
     def __init__(self, host, port):
         self.host = host
         self.port = port
+
+        self.connectionManager = QueuedConnectionManager()
+        self.connectionReader = QueuedConnectionReader(self.connectionManager, 0)
+        self.connectionWriter = ConnectionWriter(self.connectionManager, 0)
+
+        self.connection = self.connectionManager.openTCPClientConnection(host, port, 10)
+        self.connectionReader.addConnection(self.connection)
+
+        taskMgr.add(self.readerPollTask, self.uniqueName('reader-poll-task'))
+
+    def readerPollTask(self, task):
+        """
+        Continuously polls for new messages on the server.
+        """
+        while self.readerPollOnce():
+            pass
+
+        return task.cont
+
+    def readerPollOnce(self):
+        """
+        Checks for messages available to the server.
+        """
+        # Check if we have any data available:
+        dataAvailable = self.connectionReader.dataAvailable()
+        if dataAvailable:
+            # Get the data:
+            datagram = NetDatagram()
+            if self.connectionReader.getData(datagram):
+                # We got the data! We can now process it:
+                self.handleServerDatagram(datagram)
+
+        return dataAvailable
+
+    def sendUpstream(self, datagram):
+        self.connectionWriter.send(datagram, self.connection)
+
+    def handleServerDatagram(self, datagram):
+        raise NotImplementedError
