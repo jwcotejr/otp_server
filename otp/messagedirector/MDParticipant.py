e61703e6c30adef50f283e99b7bba765c906ca09
John Cote <jwcotejr@gmail.com>
2022-12-27 22:01:21 -0500

    messagedirector: Start handling control messages

--- otp/messagedirector/MDParticipant.py
+++ otp/messagedirector/MDParticipant.py
@@ -1,7 +1,31 @@
 from otp.net.NetworkClient import NetworkClient
+from otp.core import MsgTypes
 
 
 class MDParticipant(NetworkClient):
 
     def __init__(self, acceptor, connection):
         NetworkClient.__init__(self, acceptor, connection)
+
+        # A list of our subscribed channels:
+        self.channels = []
+
+    def handleClientDatagram(self, dgi):
+        """
+        Handle a datagram sent by the client.
+        """
+        msgType = dgi.getUint16()
+
+        if msgType == MsgTypes.CONTROL_SET_CHANNEL:
+            self.handleControlSetChannel(dgi)
+
+    def handleControlSetChannel(self, dgi):
+        # Get the channel we want to subscribe to:
+        channel = dgi.getUint64()
+
+        # Subscribe to the channel:
+        self.subscribeChannel(channel)
+
+    def subscribeChannel(self, channel):
+        self.channels.append(channel)
+        self.acceptor.subscribeChannel(self, channel)
