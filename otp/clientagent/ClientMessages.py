0677e3acf3dfea56fb4271f262947fb671483839
John Cote <jwcotejr@gmail.com>
2022-12-28 19:24:09 -0500

    clientagent: Work on handleClientLoginToontown

--- otp/clientagent/ClientMessages.py
+++ otp/clientagent/ClientMessages.py
@@ -3,3 +3,7 @@ CLIENT_GO_GET_LOST = 4
 
 # new toontown specific login message, adds last logged in, and if child account has parent acount
 CLIENT_LOGIN_TOONTOWN = 125
+
+# The following is a different set of numbers from above.
+# These are the sub-message types for CLIENT_LOGIN_2.
+CLIENT_LOGIN_3_DISL_TOKEN = 4  # SSL encoded blob from DISL system.
--- otp/clientagent/ToontownClient.py
+++ otp/clientagent/ToontownClient.py
@@ -18,3 +18,18 @@ class ToontownClient(Client):
         hashVal = dgi.getUint32()
         tokenType = dgi.getInt32()
         wantMagicWords = dgi.getString()
+
+        # Check if the client is the expected version:
+        if serverVersion != self.acceptor.getServerVersion():
+            self.sendDisconnect(125, 'Client version mismatch')
+            return
+
+        # Check if the client has the expected DC hash:
+        if hashVal != self.acceptor.getDcHash():
+            self.sendDisconnect(125, 'DC hash mismatch')
+            return
+
+        # Check if the client is sending the expected token type:
+        if tokenType != ClientMessages.CLIENT_LOGIN_3_DISL_TOKEN:
+            self.sendDisconnect(122, 'Token type mismatch')
+            return
