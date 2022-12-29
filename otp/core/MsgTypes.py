a0666d30bd08b52671f09fbfe62c6997ec898792
John Cote <jwcotejr@gmail.com>
2022-12-29 00:31:58 -0500

    core: Remove this

--- otp/core/MsgTypes.py
+++ otp/core/MsgTypes.py
@@ -4,9 +4,6 @@ CHANNEL_CLIENT_BROADCAST = 4014
 CONTROL_MESSAGE = 4001
 CONTROL_SET_CHANNEL = 2001
 
-# Account Messages
-ACCOUNT_GET_ACCOUNT = 3001
-
 # The ID number of the database server.  The above direct-to-dbserver
 # transactions are sent to this ID.
 DBSERVER_ID = 4003
