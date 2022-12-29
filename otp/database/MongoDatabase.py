eed456cc30bdc5e1ca756c48ac2f7858eb7aee42
John Cote <jwcotejr@gmail.com>
2022-12-29 17:53:36 -0500

    database: Connect to MongoDB

--- otp/database/MongoDatabase.py
+++ otp/database/MongoDatabase.py
@@ -1,10 +1,21 @@
 from otp.database.DatabaseBackend import DatabaseBackend
 
+from pymongo import MongoClient, uri_parser
+
 
 class MongoDatabase(DatabaseBackend):
 
     def __init__(self, server, generateMin, generateMax):
         DatabaseBackend.__init__(self, generateMin, generateMax)
+
+        # Get our database from the URI:
+        database = uri_parser.parse_uri(server)['database']
+
+        # Create our Mongo Client:
+        self.mongoClient = MongoClient(server)
+
+        # Get our MongoDB:
+        self.mongodb = self.mongoClient[database]
 
     @staticmethod
     def createFromConfig(backendConfig, generateMin, generateMax):
