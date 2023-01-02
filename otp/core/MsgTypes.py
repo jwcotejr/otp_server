98b5ae9f659e1184d49d0de29ba6d4665c26336f
John Cote <jwcotejr@gmail.com>
2023-01-01 19:06:07 -0500

    core: Add DBSERVER_CREATE_STORED_OBJECT to MsgTypes

--- otp/core/MsgTypes.py
+++ otp/core/MsgTypes.py
@@ -4,6 +4,9 @@ CHANNEL_CLIENT_BROADCAST = 4014
 CONTROL_MESSAGE = 4001
 CONTROL_SET_CHANNEL = 2001
 
+# direct-to-database-server transactions
+DBSERVER_CREATE_STORED_OBJECT = 1003
+
 # The ID number of the database server.  The above direct-to-dbserver
 # transactions are sent to this ID.
 DBSERVER_ID = 4003
