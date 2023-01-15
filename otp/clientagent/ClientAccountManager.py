33a634fe6aaed8e02bbd86deb0a25e86ad3f9b0f
John Cote <jwcotejr@gmail.com>
2023-01-14 22:14:52 -0500

    clientagent: Create dbm

--- otp/clientagent/ClientAccountManager.py
+++ otp/clientagent/ClientAccountManager.py
@@ -1,4 +1,10 @@
+import semidbm
+
+
 class ClientAccountManager:
 
     def __init__(self, acceptor):
         self.acceptor = acceptor
+
+        # Create our dbm:
+        self.dbm = semidbm.open('databases/accounts', 'c')
